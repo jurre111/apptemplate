fcbebb3309460e1938a0d8408f97feb958caad47
Jurre <188440025+jurre111@users.noreply.github.com>
2026-07-27 21:14:26 +0200

    sheet

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -2,8 +2,20 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    let fm = FileManager.default
+    @State private var showsheet = true
     var body: some View {
         Map()
+            .sheet(isPresented: $showSheet) {
+                List {
+                    Section("Test") {
+                        Text("Test")
+                    }
+                }
+                .presentationDetents([.height(80), .medium, .large])
+                .presentationBackgroundInteraction(.enabled(upTrough: .medium))
+                .interactiveDismissDisabled()
+                
     }
 }
 

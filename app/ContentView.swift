68733602a5ba26dad897f057705d67f75cb41110
Jurre <jurredijkstra@mijnheem.nl>
2026-07-28 12:30:50 +0000

    test

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -4,8 +4,11 @@ import MapKit
 struct ContentView: View {
     let fm = FileManager.default
     @Namespace private var pineMapScope
-    @AppStorage("alwaysShowCompass") private var alwaysShowCompass: Bool = false
-    @AppStorage("alwaysShowPitchToggle") private var alwaysShowPitchToggle: Bool = false
+    @AppStorage("showCompass") private var showCompass: Bool?
+    @AppStorage("showPitchToggle") private var showPitchToggle: Bool
+	@AppStorage("showScale") private var showScale: Bool?
+	@AppStorage("showLocation") private var showLocation: Bool
+	@AppStorage("showZoomButtons") private var showZoomButtons: Bool
     @State private var showSheet: Bool = true
     var body: some View {
         Map(scope: pineMapScope)
@@ -20,14 +23,16 @@ struct ContentView: View {
                 }
             }
             .mapControls {
-                if alwaysShowCompass {
-                    MapCompass()
-                        .mapControlVisibility(.visible)
-                }
-                if alwaysShowPitchToggle {
-                    MapPitchToggle()
-                }
-                
+                MapCompass()
+                    .mapControlVisibility(showCompass == nil ? .automatic : (showCompass ? .visible : .hidden))
+                MapPitchToggle()
+                    .mapControlVisibility(showPitchToggle ? .visible: .hidden)
+                MapScaleView()
+                    .mapControlVisibility(showScale == nil ? .automatic : (showScale ? .visible: .hidden))
+                MapUserLocationButton()
+                    .mapControlVisibility(showLocation ? .visible: .hidden)
+                MapZoomStepper()
+                    .mapControlVisibility(showZoomButtons ? .visible: .hidden)
             }
             .sheet(isPresented: $showSheet) {
                 SettingsView()
--- app/SettingsView.swift
+++ app/SettingsView.swift
@@ -1,16 +1,30 @@
 import SwiftUI
 
 struct SettingsView: View {
-	@AppStorage("alwaysShowCompass") private var alwaysShowCompass: Bool = false
-    @AppStorage("alwaysShowPitchToggle") private var alwaysShowPitchToggle: Bool = false
+	@AppStorage("showCompass") private var showCompass: Bool?
+    @AppStorage("showPitchToggle") private var showPitchToggle: Bool = false
+	@AppStorage("showScale") private var showScale: Bool?
+	@AppStorage("showLocation") private var showLocation: Bool = false
+	@AppStorage("showZoomButtons") private var showZoomButtons: Bool = false
 	var body: some View {
 		NavigationStack {
 			List {
 				NavigationLink("User Interface") {
 					List {
 						Section("Map UI") {
-							Toggle("Show Compass", isOn: $alwaysShowCompass)
-							Toggle("Show Pitch Toggle", isOn: $alwaysShowPitchToggle)
+							Picker("Compass", selection: $showCompass) {
+								Text("On").tag(true)
+								Text("Off").tag(false)
+								Text("Automatic").tag(nil)
+							}
+							Picker("Scale", selection: $showScale) {
+								Text("On").tag(true)
+								Text("Off").tag(false)
+								Text("Automatic").tag(nil)
+							}
+							Toggle("Show Pitch Toggle", isOn: $showPitchToggle)
+							Toggle("Show Location", isOn: $showLocation)
+							Toggle("Show Zoom Buttons", isOn: $showZoomButtons)
 						}
 					}
 				}

952750d53884724ca33b2910d2aa33c3f5f83b98
jurre111 <jurredijkstra@mijnheem.nl>
2026-07-29 18:08:56 +0200

    fix?

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -56,9 +56,8 @@ struct ContentView: View {
                 SettingsView()
                 .presentationBackground {
                     ZStack {
-                        Material.regular
-                        Color(.systemBackground)
-                            .opacity(0.6)
+                        Rectangle().fill(.regularMaterial)
+                        Color(.systemBackground).opacity(0.6)
                     }
                 }
                 .presentationDetents([.height(70), .medium, .large])

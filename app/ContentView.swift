93c5085e8ce4fd765bebbd69d9a3095c41d025f0
jurre111 <jurredijkstra@mijnheem.nl>
2026-08-01 21:19:52 +0200

    huh?

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -61,8 +61,8 @@ struct ContentView: View {
                 //         Color(.systemBackground).opacity(0.4)
                 //     }
                 // }
-                .presentationDetents([.height(70), .height(262), .large])
-                .presentationBackgroundInteraction(.enabled(upThrough: .height(262)))
+                .presentationDetents([.height(70), .height(400), .large])
+                .presentationBackgroundInteraction(.enabled(upThrough: .height(400)))
                 .interactiveDismissDisabled()
             }
     }
